6f540b0fffc34c6a909ce4d1788f1dff65d18fb2
Nihar Joshi <nsj0596@gmail.com>
2021-12-03 16:04:54 -0600

    Removed Kafka write stream
    Signed-off-by: Nihar Joshi <nsj0596@gmail.com>

--- src/main/scala/AlertSystem.scala
+++ src/main/scala/AlertSystem.scala
@@ -9,22 +9,13 @@ object AlertSystem {
 
     val df = spark.readStream.format("kafka").
       option("kafka.bootstrap.servers", "b-3.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-2.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-1.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092").
+      option("subscribe", topicName).
       option("kafka.security.protocol", "PLAINTEXT").
-      option("subscribe", topicName).load()
+      option("startingOffset","earliest").load()
 
     df.printSchema()
 
     val messages = df.selectExpr("CAST(value AS STRING)")
 
-    val query = messages.writeStream.
-      option("path", "data.txt").
-      option("checkpointLocation", "checkpoint.txt").
-      format("text").start()
-
-    query.awaitTermination()
-    query.stop()
-
-    println(query)
-  }
-
+    messages.collect.foreach(println)
 }
