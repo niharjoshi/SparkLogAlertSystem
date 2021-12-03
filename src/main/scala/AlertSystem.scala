e45fd336f28c1be5e6c14d542cb6e0fdee476045
Nihar Joshi <nsj0596@gmail.com>
2021-12-03 16:06:18 -0600

    Added missing brace
    Signed-off-by: Nihar Joshi <nsj0596@gmail.com>

--- src/main/scala/AlertSystem.scala
+++ src/main/scala/AlertSystem.scala
@@ -11,11 +11,12 @@ object AlertSystem {
       option("kafka.bootstrap.servers", "b-3.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-2.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-1.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092").
       option("subscribe", topicName).
       option("kafka.security.protocol", "PLAINTEXT").
-      option("startingOffset","earliest").load()
+      option("startingOffset", "earliest").load()
 
     df.printSchema()
 
     val messages = df.selectExpr("CAST(value AS STRING)")
 
     messages.collect.foreach(println)
+  }
 }
