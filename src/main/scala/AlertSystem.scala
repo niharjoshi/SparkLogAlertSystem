4d7eddfeda91f61a2d16474bae158dc75daaf8b1
Nihar Joshi <nsj0596@gmail.com>
2021-12-03 17:28:38 -0600

    Testing changes
    Signed-off-by: Nihar Joshi <nsj0596@gmail.com>

--- src/main/scala/AlertSystem.scala
+++ src/main/scala/AlertSystem.scala
@@ -22,11 +22,11 @@ object AlertSystem {
 
     val messages = df.selectExpr("CAST(value AS STRING)")
 
-    val modified_messages = messages.select(split(col("value"), " ").
-      getItem(0).as("Timestamp").
-      getItem(2).as("Level").
-      getItem(3).as("Source").
-      getItem(5).as("Message")
+    val modified_messages = messages.select(
+      split(col("value"), " ").getItem(0).as("Timestamp"),
+      split(col("value"), " ").getItem(2).as("Level"),
+      split(col("value"), " ").getItem(3).as("Source"),
+      split(col("value"), " ").getItem(5).as("Message")
     )
 
     modified_messages.writeStream.outputMode("append").format("console").start.awaitTermination()
