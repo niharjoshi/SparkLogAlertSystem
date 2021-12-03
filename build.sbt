af05cea220310b952b8276ba840006e325bc57aa
Nihar Joshi <nsj0596@gmail.com>
2021-12-03 01:55:13 -0600

    Added Kafka streaming support
    Signed-off-by: Nihar Joshi <nsj0596@gmail.com>

--- build.sbt
+++ build.sbt
@@ -3,3 +3,13 @@ name := "SparkLogAlertSystem"
 version := "0.1"
 
 scalaVersion := "2.13.7"
+
+val sparkVersion = "2.4.8"
+
+libraryDependencies += "org.apache.spark" % "spark-core_2.11" % sparkVersion
+
+libraryDependencies += "org.apache.spark" % "spark-sql_2.11" % sparkVersion
+
+libraryDependencies += "org.apache.spark" % "spark-streaming-kafka-0-10_2.11" % sparkVersion
+
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "3.0.0"
---
+++ src/main/scala/AlertSystem.scala
@@ -0,0 +1,27 @@
+import org.apache.spark.sql.SparkSession
+
+object AlertSystem {
+
+  def main(args: Array[String]) = {
+    val spark = SparkSession.builder.getOrCreate()
+
+    val topicName = "logs"
+
+    val df = spark.readStream.format("kafka").
+      option("kafka.bootstrap.servers", "b-3.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-2.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-1.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092").
+      option("kafka.security.protocol", "PLAINTEXT").
+      option("subscribe", topicName).load()
+
+    df.printSchema()
+
+    val messages = df.selectExpr("CAST(value AS STRING)")
+
+    val query = messages.writeStream.format("text").start()
+
+    query.awaitTermination()
+    query.stop()
+
+    println(query)
+  }
+
+}
