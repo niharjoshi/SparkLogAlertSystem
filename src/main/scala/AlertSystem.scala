69b7a831fdf12676830fac80c887bd9416cf2f4a
Nihar Joshi <nsj0596@gmail.com>
2021-12-03 18:36:46 -0600

    Testing changes
    Signed-off-by: Nihar Joshi <nsj0596@gmail.com>

--- src/main/scala/AlertSystem.scala
+++ src/main/scala/AlertSystem.scala
@@ -16,24 +16,20 @@ object AlertSystem {
       option("kafka.bootstrap.servers", "b-3.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-2.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092,b-1.logfilegeneratorkafkac.c9jlb9.c7.kafka.us-east-2.amazonaws.com:9092").
       option("subscribe", topicName).
       option("kafka.security.protocol", "PLAINTEXT").
-      load()
-
-    df.printSchema()
-
-    val messages = df.selectExpr("CAST(value AS STRING)")
-
-    val modified_messages = messages.select(
+      load().selectExpr("CAST(value AS STRING)").select(
       split(col("value"), " ").getItem(0).as("Timestamp"),
       split(col("value"), " ").getItem(2).as("Level"),
       split(col("value"), " ").getItem(3).as("Source"),
       split(col("value"), " ").getItem(5).as("Message")
     )
 
-//    modified_messages.writeStream.outputMode("append").format("console").start.awaitTermination
+    val query = df.writeStream.outputMode("append").format("console").start
 
-    val mail = createHtmlEmailBody(modified_messages)
+    val mail = createHtmlEmailBody(df)
 
     println(mail)
+
+    query.awaitTermination()
 
   }
 
